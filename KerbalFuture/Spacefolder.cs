f4d9274c0a101cf7d8b3a32505f26b4853530028
Benjamin Kerman <bcronin720@gmail.com>
2017-06-22 20:22:32 -0700

    Update Spacefolder.cs

--- KerbalFuture/Spacefolder.cs
+++ KerbalFuture/Spacefolder.cs
@@ -14,14 +14,24 @@ namespace KerbalFuture
 			return System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
 		}
 	}
+	class SpaceFolderVslChecks : MonoBehavior
+	{
+		List<Part> Vessel.Parts = new List<Part> Vessel.Parts;
+		public bool SpaceFolderWarpCheck()
+		{
+			if (Vessel.
+		}
+	}
 	class FlightDrive : VesselModule
 	{
 		Vector3 vslObtVel;
 		public void FixedUpdate()
 		{
-			if(HighLogic.LoadedSceneIsFlight)
+			//need a part list here...
+			if((HighLogic.LoadedSceneIsFlight) && (SpaceFolderWarpCheck()))
 			{
 				vslObtVel = Vessel.GetObtVelocity();
+				if (SpacefolderWarpCheck(true))
 			}
 		}
 	}
