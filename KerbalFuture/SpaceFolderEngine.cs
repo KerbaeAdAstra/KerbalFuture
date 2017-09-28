82583fba57c2a565e320668379536e6500329ccb
Benjamin Kerman <bcronin720@gmail.com>
2017-09-28 22:17:55 +0000

    Add Engine

---
+++ KerbalFuture/SpaceFolderEngine.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using KSP;
+
+namespace KerbalFuture
+{
+	class SpaceFolderEngine : PartModule
+	{
+		double holeDiameter;
+	}
+}
