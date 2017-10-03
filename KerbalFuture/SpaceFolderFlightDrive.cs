afa11beacb0c2d74067295003d42a2cba0492f1a
Benjamin Kerman <benjamin@bakedpotato.localdomain>
2017-10-02 18:18:50 -0700

    work on vessel warp

--- KerbalFuture/SpaceFolderFlightDrive.cs
+++ KerbalFuture/SpaceFolderFlightDrive.cs
@@ -13,15 +13,31 @@ namespace KerbalFuture
 		private static Vector3d vesPos;
 		private static double vesHeight;
 		private static CelestialBody vesBody;
-		
+		private static CelestialBody warpBody;
+
 		public void WarpVessel()
 		{
 			if(WarpIsGo())
 			{
 				vesPos = this.Vessel.GetWorldPosition3D();
 				vesBody = this.Vessel.mainBody;
-				vesHeight = this.Vessel.alitiude + vesBody.Radius;
+				vesHeight = (this.Vessel.alitiude + vesBody.Radius);
+				warpBody = GUIChosenBody();
+				
+				
 			}
+		}
+		private double GetVesselLongPos(Vector3d pos)
+		{
+			return this.Vessel.GetLongitude(pos, false);
+		}
+		private double GetVesselLatPos(Vector3d pos)
+		{
+			return this.Vessel.GetLatitude(pos, false);
+		}
+		private void CalculateGravPot(CelestialBody cb)
+		{
+			cb.
 		}
 	}
 }
