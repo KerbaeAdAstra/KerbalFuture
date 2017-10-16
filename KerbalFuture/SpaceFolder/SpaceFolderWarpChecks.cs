c2e6c846ddf4023ed44af64b93c6ff8d4b176753
Benjamin Kerman <benjamin@bakedpotato.localdomain>
2017-10-16 13:26:12 -0700

    sfd: push for work on other computer

--- KerbalFuture/SpaceFolder/SpaceFolderWarpChecks.cs
+++ KerbalFuture/SpaceFolder/SpaceFolderWarpChecks.cs
@@ -6,38 +6,69 @@ namespace SpaceFolder
 	class SpaceFolderWarpChecks : MonoBehaviour
 	{
 		static bool goodToGo;
-		static double vesselDiameter;
+		static float vesselDiameter;
 		static var vesData;
+		static float[] warpEngineSizes;
+		static float maxWarpHoleSize;
+		static double spaciofibrinNeeded;
 		
-		public static double VesselDiameter(Vessel vessel)
+		[KSPField(isPersistant = true)]
+		static float quadCoefficent;
+
+		[KSPField(isPersistant = true)]
+		static float quadH;
+
+		[KSPField(isPersistant = true)]
+		static float quadK;
+
+		public static double SpaciofibrinWarpCalc(float[] engineSizes)
 		{
-			//TODO
+			double returnAmount;
+			foreach(float f in engineSizes)
+			{
+				returnAmount += QuadCalc(f);
+			}
+			return returnAmount;
 		}
-		public static void InitiateWarpCheck() //called by GUI, sets bool goodToGo
+		private static double QuadCalc(float engineSize)
 		{
+			engineSize = (double)engineSize;
+			quadH = (double)quadH;
+			quadK = (double)quadK;
+			quadCoefficent = (double)quadCoefficent;
+			return (quadCoefficent * Math.Pow((engineSize - quadH), 2) + quadK);
+		}
+		public static void InitiateWarpCheck(float[] engineSizes, float vesDiameter) //called by GUI, sets bool goodToGo
+		{
+			warpEngineSizes.Clear();
+			vesselDiameter = vesDiameter;
+			goodToGo = false;
+			foreach(float f in engineSizes)
+			{
+				warpEngineSizes.Add(f);
+			}
 			//constructs a new VesselData class
 			vesData = new VesselData(FlightGlobals.activeVessel);
 			int spaceFolderClassID = SpaceFolderEngine.ModuleClassID();
 			//Checks if the current vessel has a SpaceFolderEngine
-			if (!vesData.VesselContainsModule(FlightGlobals.activeVessel, SpaceFolderEngine.spaceFolderClassID))
+			if (!vesData.VesselContainsModule(FlightGlobals.activeVessel, spaceFolderClassID))
 			{
 				return;
 			}
 			//Checks the size of the SpaceFolderEngine module
 			
 			//If vessel !have Spatiofibrin, return
-			//if (ResourceAmountOnVessel("Spatiofibrin", CurrentVessel()) <= 
-			//if (ResourceAmountOnVessel("Spatiofibrin", FlightGlobals.activeVessel) < 
+			spaciofibrinNeeded = SpaciofibrinWarpCalc(engineSizes);
+			if(ResourceAmountOnVessel("Spaciofibrin", FlightGlobals.activeVessel) < spaciofibrinNeeded)
+			{
+				return;
+			}
 			// ResourceAmountNeeded())
 			//	return;
 			// If warpDrive.diameter < vesselSize, return
 			//
 			// WarpVessel();
-		}
-		public static bool WarpIsGo()
-		{
-			InitiateWarpCheck();
-			return goodToGo;
+			goodToGo = true;
 		}
 	}
 }
