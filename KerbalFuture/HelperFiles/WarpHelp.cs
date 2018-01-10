ab27c87d4b26a0ab16c87918de797ea8adbc967f
Benjamin Kerman <bcronin720@gmail.com>
2018-01-10 00:06:49 +0000

    Fix non-existant ref vars in function calls.

--- KerbalFuture/HelperFiles/WarpHelp.cs
+++ KerbalFuture/HelperFiles/WarpHelp.cs
@@ -14,8 +14,8 @@ namespace Hlpr
 		public static double Distance(Vector3d v1, Vector3d v2)
 		{
 			double dis, x1, x2, y1, y2, z1, z2;
-			ConvertVector3dToXYZCoords(v1, x1, y1, z1);
-			ConvertVector3dToXYZCoords(v2, x2, y2, z2);
+			ConvertVector3dToXYZCoords(v1, ref x1, ref y1, ref z1);
+			ConvertVector3dToXYZCoords(v2, ref x2, ref y2, ref z2);
 			dis = Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2) + Math.Pow(z1-z2,2);
 			dis = Math.Pow(dis,0.5);
 			return dis;
