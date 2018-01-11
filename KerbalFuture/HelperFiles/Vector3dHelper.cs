4233c97d0cadc1e4b9b62720442f1ec866b5ad4c
Benjamin Kerman <bcronin720@gmail.com>
2018-01-11 16:41:33 +0000

    Formatting

--- KerbalFuture/HelperFiles/Vector3dHelper.cs
+++ KerbalFuture/HelperFiles/Vector3dHelper.cs
@@ -4,18 +4,18 @@ namespace Hlpr
 	{
 		double v3dX, v3dY, v3dZ;
 
-		public static void ConvertVector3dToXYZCoords(Vector3d v3d, ref double x,
-													  ref double y, ref double z)
+		public static void ConvertVector3dToXYZCoords(Vector3d v3d, ref double x, 
+		    ref double y, ref double z)
 		{
 			x = v3d.x;
 			y = v3d.y;
 			z = v3d.z;
 		}
 		public static Vector3d ConvertXYZCoordsToVector3d(double x, double y,
-														  double z)
+			double z)
 		=> new Vector3d(x, y, z);
 		public static void ConvertXYZCoordsToVector3d(double x, double y,
-													  double z, ref Vector3d v3d)
+			double z, ref Vector3d v3d)
 		=> v3d = new Vector3d(x, y, z);
 		public double Vector3dX() => v3dX;
 		public double Vector3dY() => v3dY;
