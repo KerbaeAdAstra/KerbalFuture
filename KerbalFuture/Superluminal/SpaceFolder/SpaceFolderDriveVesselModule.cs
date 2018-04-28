b8e6271e6ccd58ec8681a76ad16aa2ddfacae058
Benjamin Cronin <bcronin720@gmail.com>
2018-04-28 15:44:03 -0700

    utils: added UnitTests.cs, sfd: added Awake() and internal testing WarpVessel

--- KerbalFuture/Superluminal/SpaceFolder/SpaceFolderDriveVesselModule.cs
+++ KerbalFuture/Superluminal/SpaceFolder/SpaceFolderDriveVesselModule.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 
 namespace KerbalFuture.Superluminal.SpaceFolder
 {
-	[KSPAddon(KSPAddon.Startup.Flight, true)]
-	public class SpaceFolderDriveVesselModule : VesselModule
-	{
-		// 1 Electric Charge is equal to 1kJ\!
-		
-		// List of of drives on the vessel, updated/created on warp
-		List<Part> driveList = new List<Part>();
-		// Dictionary of the part and its respective participation in the warp
-		Dictionary<Part, double> partECAmount = new Dictionary<Part, double>();
-        public void Awake()
+    [KSPAddon(KSPAddon.Startup.Flight, true)]
+    public class SpaceFolderDriveVesselModule : VesselModule
+    {
+        // 1 Electric Charge is equal to 1kJ\!
+
+        // List of of drives on the vessel, updated/created on warp
+        List<Part> driveList = new List<Part>();
+        // Dictionary of the part and its respective participation in the warp
+        Dictionary<Part, double> partECAmount = new Dictionary<Part, double>();
+        UnitTests ut;
+        protected override void OnStart()
         {
             Debug.Log("[KF] Startup for " + this.Vessel.name.ToString());
+            ut = new UnitTests(this);
         }
         //Internal testing code
         internal bool WarpVessel(Vector3d location)
@@ -24,60 +26,59 @@ namespace KerbalFuture.Superluminal.SpaceFolder
             Debug.Log("[KF] Internal Warp Triggered!");
             vessel.SetPosition(location);
             return true;
-        }
-        // Warps the vessel, using resources
+        }        // Warps the vessel, using resources
         public bool WarpVessel(SpaceFolderWarpData warpData, out int fault)
-		{
-			driveList = WarpHelp.PartsWithModule(Vessel, new ModuleSpaceFolderEngine());
-			int internFault = fault = SpaceFolderWarpChecks.WarpAvailable(warpData, Vessel);
-			if (internFault != 0)
-			{
-				return false;
-			}
-			UseWarpResources();
-			vessel.SetPosition(warpData.WarpLocation);
-			DistributeHeat();
-			return true;
-		}
-		// Uses resources from the drives in driveList
-		private void UseWarpResources()
-		{
-			List<SpaceFolderDriveData> driveData = new List<SpaceFolderDriveData>();
-			foreach (Part p in driveList)
-			{
-				driveData.Add(((ModuleSpaceFolderEngine)p.Modules["ModuleSpaceFolderEngine"]).PartDriveData);
-			}
-			foreach (SpaceFolderDriveData d in driveData)
-			{
-				double tempEC = MainResourceWarpCalc(d.Diameter, d.Multiplier);
-				partECAmount.Add(d.DriveDataPart, tempEC);
-				WarpHelp.UseResource(d.DriveDataPart, tempEC, d.MainResource);
-			}
-		}
-		// Heat distribution for after warp, using ModuleCoreHeat put in place by a MM patch
-		private void DistributeHeat()
-		{
-			// Adds heat
-			foreach (KeyValuePair<Part, double> kvp in partECAmount)
-			{
-				bool hadModuleCoreHeat = true;
-				// Iff MM doesn't work or this engine for whatever reason doesn't have a ModuleCoreHeat, we add one
-				if (!kvp.Key.Modules.Contains("ModuleCoreHeat"))
-				{
-					hadModuleCoreHeat = false;
-					kvp.Key.AddModule("ModuleCoreHeat", true);
-				}
-				// p.Modules.GetModule<ModuleSpaceFolderEngine>.PartDriveData
-				((ModuleCoreHeat)kvp.Key.Modules["ModuleCoreHeat"]).AddEnergyToCore(kvp.Value);
-				// Removes ModuleCoreHeat if the part didn't already have it
-				if (hadModuleCoreHeat)
-				{
-					kvp.Key.RemoveModule(kvp.Key.Modules["ModuleCoreHeat"]);
-				}
-			}
-		}
+        {
+            driveList = WarpHelp.PartsWithModule(Vessel, new ModuleSpaceFolderEngine());
+            int internFault = fault = SpaceFolderWarpChecks.WarpAvailable(warpData, Vessel);
+            if (internFault != 0)
+            {
+                return false;
+            }
+            UseWarpResources();
+            vessel.SetPosition(warpData.WarpLocation);
+            DistributeHeat();
+            return true;
+        }
+        // Uses resources from the drives in driveList
+        private void UseWarpResources()
+        {
+            List<SpaceFolderDriveData> driveData = new List<SpaceFolderDriveData>();
+            foreach (Part p in driveList)
+            {
+                driveData.Add(((ModuleSpaceFolderEngine)p.Modules["ModuleSpaceFolderEngine"]).PartDriveData);
+            }
+            foreach (SpaceFolderDriveData d in driveData)
+            {
+                double tempEC = MainResourceWarpCalc(d.Diameter, d.Multiplier);
+                partECAmount.Add(d.DriveDataPart, tempEC);
+                WarpHelp.UseResource(d.DriveDataPart, tempEC, d.MainResource);
+            }
+        }
+        // Heat distribution for after warp, using ModuleCoreHeat put in place by a MM patch
+        private void DistributeHeat()
+        {
+            // Adds heat
+            foreach (KeyValuePair<Part, double> kvp in partECAmount)
+            {
+                bool hadModuleCoreHeat = true;
+                // Iff MM doesn't work or this engine for whatever reason doesn't have a ModuleCoreHeat, we add one
+                if (!kvp.Key.Modules.Contains("ModuleCoreHeat"))
+                {
+                    hadModuleCoreHeat = false;
+                    kvp.Key.AddModule("ModuleCoreHeat", true);
+                }
+                // p.Modules.GetModule<ModuleSpaceFolderEngine>.PartDriveData
+                ((ModuleCoreHeat)kvp.Key.Modules["ModuleCoreHeat"]).AddEnergyToCore(kvp.Value);
+                // Removes ModuleCoreHeat if the part didn't already have it
+                if (hadModuleCoreHeat)
+                {
+                    kvp.Key.RemoveModule(kvp.Key.Modules["ModuleCoreHeat"]);
+                }
+            }
+        }
         //Calculates the amount of main resource used
         private double MainResourceWarpCalc(double diameter, double multiplier)
-	        => Math.Pow(Math.E, diameter * multiplier / 5)* 300;
+            => Math.Pow(Math.E, diameter * multiplier / 5) * 300;
     }
 }
---
+++ KerbalFuture/Utils/UnitTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using KerbalFuture.Superluminal.SpaceFolder;
+using UnityEngine;
+
+namespace KerbalFuture.Utils
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)] //starts every time player enters flight scene
+    class UnitTests : MonoBehaviour
+    {
+        public UnitTests(SpaceFolderDriveVesselModule vm)
+        {
+            vesselModule = vm;
+        }
+        public SpaceFolderDriveVesselModule vesselModule;
+        public void Update()
+        {
+            if(Input.GetKey(KeyCode.U) && (Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt)) && vesselModule != null)
+            {
+                Debug.Log("[KF] Unit test 'Warp' triggered");
+                vesselModule.WarpVessel(new Vector3d(40000, 2100000, 500000000));
+            }
+            if(Input.GetKey(KeyCode.O) && (Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt)))
+            {
+                Debug.Log("[KF] Unit test 'WarpChecks' triggered");
+            }
+        }
+    }
+}
