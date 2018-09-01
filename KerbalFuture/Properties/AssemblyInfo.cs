8b0fb7d8514eceb9c84f80c7d47dce5c596c607e
Benjamin Cronin <bcronin720@gmail.com>
2018-09-01 16:51:30 -0700

    added AssemblyInfo file to ensure that AT_Utils 1.6 or higher are present when loading KSP, otherwise KSP won't load the plugin, preventing all kinds of mean and nasty things.

---
+++ KerbalFuture/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("KerbalFuture")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Kerbae ad Astra")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("0.1")]
+[assembly: KSPAssembly("KerbalFuture", 0, 1)]
+
+[assembly: KSPAssemblyDependency("AT_Utils", 1, 6)]
